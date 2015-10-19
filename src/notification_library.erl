407206c7c79d784a7e878c4309d9fe11c0002018
Don Branson <don.branson@gmail.com>
2015-10-19 18:25:35 -0500

    Take token and message as strings, not binaries.

--- src/notification_library.erl
+++ src/notification_library.erl
@@ -4,4 +4,4 @@
 
 notify(Message) ->
   {ok, Token} = application:get_env(mousetrap, slack_token),
-  slacker_chat:post_message(Token, <<"#mousetrap">>, Message, <<"mousetrap">>).
+  slacker_chat:post_message(list_to_binary(Token), <<"#mousetrap">>, list_to_binary(Message), <<"mousetrap">>).
--- test/notification_library_tests.erl
+++ test/notification_library_tests.erl
@@ -3,11 +3,11 @@
 -include_lib("eunit/include/eunit.hrl").
 
 fixture_startup_test() ->
-  application:set_env(mousetrap, slack_token, test_token),
+  application:set_env(mousetrap, slack_token, "test_token"),
   meck:new(slacker, [unstick]),
   meck:expect(slacker_chat, post_message, 4, {ok, [long_list]}).
 
 notify_forwards_to_slacker_test() ->
-  TestMessage = <<"Test Message">>,
+  TestMessage = "Test Message",
   notification_library:notify(TestMessage),
-  ?assert(meck:called(slacker_chat, post_message, [test_token, <<"#mousetrap">>, TestMessage, <<"mousetrap">>])).
+  ?assert(meck:called(slacker_chat, post_message, [<<"test_token">>, <<"#mousetrap">>, <<"Test Message">>, <<"mousetrap">>])).
