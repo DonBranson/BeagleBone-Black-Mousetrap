f3cff3d290c96e4aaca1816d3cd7ad5bf7d6ec9b
Don Branson <don.branson@gmail.com>
2015-10-20 18:53:56 -0500

    Test start of mousetrap supervisor.

--- test/mousetrap_app_tests.erl
+++ test/mousetrap_app_tests.erl
@@ -4,8 +4,15 @@
 
 fixture_startup_test() ->
   application:set_env(mousetrap, slack_token, "test_token"),
-  meck:expect(notification_library, notify, 1, {ok, 200, [long_list]}).
+  meck:expect(notification_library, notify, 1, {ok, 200, [long_list]}),
+  meck:expect(mousetrap_sup, start_link, 0, {ok, pid}).
 
-startup_message_sent_test() ->
+start_sends_notification_test() ->
   mousetrap_app:start("", ""),
   ?assert(meck:called(notification_library, notify, ["Mousetrap starting"])).
+
+start_starts_mousetrap_supervisor_test() ->
+  ?assert(meck:called(mousetrap_sup, start_link, [])).
+
+fixture_teardown_test() ->
+  meck:unload().
