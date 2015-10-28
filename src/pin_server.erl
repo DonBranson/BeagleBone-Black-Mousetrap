5145c523d153a20fc168ef012ed93ff687a8418b
Don Branson <don.branson@gmail.com>
2015-10-28 07:37:12 -0500

    Pin servers should notify when starting. Pin servers should initialize pins when starting.

--- src/pin_server.erl
+++ src/pin_server.erl
@@ -6,22 +6,27 @@
 -export([start_link/0, init/1, terminate/2, handle_call/3, handle_info/2, code_change/3, handle_cast/2]).
 
 start_link() ->
-	gen_server:start_link(?MODULE, [], []).
+  gen_server:start_link(?MODULE, [], []).
 
 init(_Arguments) ->
-	{ok, running}.
+  {ok, running}.
 
-handle_cast(_Msg, State) ->
-	{noreply, State}.
+handle_cast({start, PinServerId, Pin, _QuietSeconds, Client}, State) ->
+  notification_library:notify(format_message("Start ~p watching pin ~p and notifying ~p", [PinServerId, Pin, Client])),
+  pin_library:initialize_pins([Pin]),
+  {noreply, State}.
 
 terminate(_Reason, _State) ->
-	ok.
+  ok.
 
 handle_call(_Request, _From, State) ->
-	{reply, ignored, State}.
+  {reply, ignored, State}.
 
 handle_info(_Info, State) ->
-	{noreply, State}.
+  {noreply, State}.
 
 code_change(_OldVsn, State, _Extra) ->
-	{ok, State}.
+  {ok, State}.
+
+format_message(Str, Args) ->
+  lists:flatten(io_lib:format(Str, Args)).
--- test/pin_server_tests.erl
+++ test/pin_server_tests.erl
@@ -4,6 +4,7 @@
 -include_lib("../src/pin_record.hrl").
 
 fixture_startup_test() ->
+  meck:expect(pin_library, initialize_pins, 1, ok),
   meck:expect(notification_library, notify, 1, {ok, 200, [long_list]}).
 
 init_returns_ok_test() ->
@@ -13,6 +14,16 @@ init_returns_ok_test() ->
 start_link_delegates_test() ->
   {ok, Actual} = pin_server:start_link(),
   ?assert(is_pid(Actual)).
+
+handle_cast_sends_notification_test() ->
+  Pin = #pin{bank = gpio0, bank_pin = 5},
+  {noreply, _LoopData} = pin_server:handle_cast({start, pin_server_5, Pin, 60, pin_server_tests}, []),
+  ?assert(meck:called(notification_library, notify, ["Start pin_server_5 watching pin {pin,gpio0,5,undefined} and notifying pin_server_tests"])).
+
+handle_cast_calls_initialize_pins_test() ->
+  Pin = #pin{bank = gpio0, bank_pin = 5},
+  pin_server:handle_cast({start, pin_server_5, Pin, 60, pin_server_tests}, []),
+  ?assert(meck:called(pin_library, initialize_pins, [[Pin]])).
 
 terminate_returns_ok_test() ->
   Actual = pin_server:terminate(reason, state),
