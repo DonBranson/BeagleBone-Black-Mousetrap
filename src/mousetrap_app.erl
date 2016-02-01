9f5a0a11cd545750324b3c91b073c684cf3916c7
Don Branson <don.branson@gmail.com>
2016-02-01 13:02:59 -0600

    Extract a function.

--- src/mousetrap_app.erl
+++ src/mousetrap_app.erl
@@ -4,12 +4,15 @@
 -export([start/2, stop/1]).
 
 start(_Type, _Args) ->
-  [{loaded,LoadedModuleList}, _, _, _, _, _ ] = application:info(),
-  {mousetrap, _Description, Version} = lists:keyfind(mousetrap, 1, LoadedModuleList),
-  notification_library:notify("@channel Mousetrap " ++ Version ++ " starting"),
+	notification_library:notify("@channel Mousetrap " ++ get_application_version() ++ " starting"),
 	mousetrap_sup:start_link().
 
 stop(_State) ->
 	notification_library:notify("@channel Mousetrap stopping"),
   mousetrap_sup:stop(),
 	ok.
+
+get_application_version() ->
+	[{loaded, LoadedModuleList}, _, _, _, _, _] = application:info(),
+	{mousetrap, _Description, Version} = lists:keyfind(mousetrap, 1, LoadedModuleList),
+	Version.
